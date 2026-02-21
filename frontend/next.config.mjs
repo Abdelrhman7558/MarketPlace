b31b1cda66a82e696d310101093b88f5b8d3696b
Abdelrahman <abdelrhmanhany840@gmail.com>
2026-02-21 20:10:15 +0200

    feat: add Next.js configuration for image domains and unoptimized images.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/MarketPlace',
-    assetPrefix: '/MarketPlace/',
     images: {
         unoptimized: true,
         domains: ['images.unsplash.com', 'plus.unsplash.com'],
     },
-    trailingSlash: true,
 };
 
 export default nextConfig;
